eed12de57e1ef44de82fe622c737bea288998e31
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2019-09-30 17:20:29 -0600

    many changes to pair_snps.py

--- pair_snps.py
+++ pair_snps.py
@@ -10,18 +10,18 @@ def get_snps(mummer_snps, mummer_reference, mummer_query):
     snps[geno1] = {}
     snps[geno2] = {}
     #1,4,12,13
-    for i,l in enumerate(open(mummer_delta, "r")):
+    for i,l in enumerate(open(mummer_snps, "r")):
         l=l.rstrip('\n')
         sl = l.split()
         if i<5:
             continue
-        p1, p2, c1, c2 = (sl[1], sl[4], sl[12], sl[13])
+        p1, p2, c1, c2 = (int(sl[0]), int(sl[3]), sl[13], sl[14])
         if c1 not in snps[geno1]:
             snps[geno1][c1] = {}
         if c2 not in snps[geno2]:
             snps[geno2][c2] = {}
-        snps[geno1][c1] = int(p1)
-        snps[geno2][c2] = int(p2)
+        snps[geno1][c1][p1] = 1
+        snps[geno2][c2][p2] = 1
     return(snps)
 
 
@@ -40,21 +40,35 @@ def parse_hits(sl):
     dir2 = sl[6]
     return(r1, r2, c1, s1, c2, s2, p1, p2, dir1, dir2)
 
-def filter(c1, s1, c2, s2, p1, p2, dir1, dir2, readlen, snps):
-    r1ok = False
-    r2ok = False
-    r1end = p1 + readlen if dir1 = "+" else p1 - readlen
-    r2end = p2 + readlen if dir2 = "+" else p2 - readlen
-    r1dir = 1 if dir1 = "+" else -1
-    r2dir = 2 if dir2 = "+" else -1
+def filter(c1, s1, c2, s2, p1, p2, dir1, dir2, readlen, snps, snpsreq):
+    r1snps = 0
+    r2snps = 0
+    r1end = p1 + readlen if dir1 == "+" else p1 - readlen
+    r2end = p2 + readlen if dir2 == "+" else p2 - readlen
+    r1dir = 1 if dir1 == "+" else -1
+    r2dir = 2 if dir2 == "+" else -1
     for r1pos, r2pos in zip(range(p1,r1end,r1dir), range(p2,r2end,r2dir)):
-        if r1pos in snps[s1][c1]:
-            r1ok = True
-        if r2pos in snps[s2][c2]:
-            r2ok = True
-    return(r1ok and r2ok)
+        #sys.stdout.write("\t" + str(r1pos) + ":" + str(r2pos))
+        #try:
+        #    sys.stdout.write("\tsnps1:"+str(snps[s1][c1][r1pos]))
+        #except:
+        #    pass
+        #try:
+        #    sys.stdout.write("\tsnps2:"+str(snps[s2][c2][r2pos]))
+        #except:
+        #    pass
+        try:
+            if r1pos in snps[s1][c1]:
+                r1snps += 1
+            if r2pos in snps[s2][c2]:
+                r2snps += 1
+        except KeyError:
+            pass
+    #sys.stdout.write("\n")
+    return(r1snps >= snpsreq and r2snps >= snpsreq)
 
-def filter_file(inconn, snps):
+def filter_file(inconn, snps, readlen, snpsreq):
+    #print(snps)
     for l in inconn:
         l = l.rstrip('\n')
         if l[0] == "#":
@@ -64,13 +78,17 @@ def filter_file(inconn, snps):
         if sl[1] == "!" or sl[3] == "!":
             continue
         r1, r2, c1, s1, c2, s2, p1, p2, dir1, dir2 = parse_hits(sl)
-        if filter(c1, s1, c2, s2, p1, p2, dir1, dir2, readlen, snps):
+        #print(sl)
+        #print("c1, s1, c2, s2, p1, p2, dir1, dir2, readlen")
+        #print(c1, s1, c2, s2, p1, p2, dir1, dir2, readlen)
+        #print(filter(c1, s1, c2, s2, p1, p2, dir1, dir2, readlen, snps))
+        if filter(c1, s1, c2, s2, p1, p2, dir1, dir2, readlen, snps, snpsreq):
             print(l)
     return(None)
 
-def filter_all(inconns, snps):
+def filter_all(inconns, snps, readlen, snpsreq):
     for i in inconns:
-        filter_file(i, snps)
+        filter_file(i, snps, readlen, snpsreq)
     return(None)
 
 def main():
@@ -79,30 +97,39 @@ def main():
     
     parser.add_argument("input", nargs="*", help="One or more .pairs files to use as input (default = stdin).")
     parser.add_argument("-i", "--standard_input", help="Take standard input and other input files (default = False)", action="store_true")
-    parser.add_argument("-m", "--mummer_snps", nargs=1, help="Mummer .snps file to filter on.")
-    parser.add_argument("-q", "--mummer_query", nargs=1, help="Specify the genotype that was the mummer query in the mummer .snps file.")
-    parser.add_argument("-r", "--mummer_reference", nargs=1, help="Specify the genotype that was the mummer reference in the mummer .snps file.")
+    parser.add_argument("-m", "--mummer_snps", required=True, help="Mummer .snps file to filter on.")
+    parser.add_argument("-q", "--mummer_query", required=True, help="Specify the genotype that was the mummer query in the mummer .snps file.")
+    parser.add_argument("-r", "--mummer_reference", required=True, help="Specify the genotype that was the mummer reference in the mummer .snps file.")
+    parser.add_argument("-l", "--read_length", help="Specify the length of the sequencing reads. Default = 125bp.")
+    parser.add_argument("-s", "--snps_required", help="Number of SNPs required to consider a read discriminable. Default = 1.")
 
     args = parser.parse_args()
 
     inconns = []
     mummer_query = ""
     mummer_reference = ""
+    mummer_snps = ""
+    readlen = 125
+    snpsreq = 1
     if args.standard_input or not args.input:
         inconns.append(sys.stdin)
     if args.input:
         for i in args.input:
             inconns.append(open(i, "r"))
     if args.mummer_query:
-        mummer_query = args.mummer_query[0]
+        mummer_query = args.mummer_query
     if args.mummer_reference:
-        mummer_reference = args.mummer_reference[0]
+        mummer_reference = args.mummer_reference
     if args.mummer_snps:
         mummer_snps = args.mummer_snps
+    if args.read_length:
+        readlen = int(args.read_length)
+    if args.snps_required:
+        snpsreq = int(args.snps_required)
 
-    snps = get_snps(mummer_snps)
+    snps = get_snps(mummer_snps, mummer_reference, mummer_query)
     
-    filter_all(inconns, snps)
+    filter_all(inconns, snps, readlen, snpsreq)
     
     for i in inconns:
         i.close()
