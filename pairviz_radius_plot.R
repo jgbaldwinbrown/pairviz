66fa088e25fbfe703d7f7559ca06a5a01f9bb3de
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-05-24 15:23:14 -0600

    named pairviz_radius_plot colors

--- pairviz_radius_plot.R
+++ pairviz_radius_plot.R
@@ -12,6 +12,10 @@ main = function() {
     plot = ggplot(data=data,
             mapping = aes(x=Distance, y=Value, color=Variable)) +
         geom_smooth() +
+        scale_color_discrete(
+            breaks=c("pair_count", "pair_prop", "self_count", "uninf_count"),
+            labels = c("Paired reads", "Proportion paired", "Self-interaction reads", "Uninformative")
+        ) +
         theme_bw()
     res_scale = 100
     png(outpath, width=8*res_scale, height=6*res_scale, res=res_scale)
